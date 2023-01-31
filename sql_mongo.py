1f8f25b16487fe7805f93f91784c028ae2b5b7af
Prasun Paul <94418947+prasunpaul315@users.noreply.github.com>
2023-01-31 10:01:09 +0530

    Update sql_mongo.py

--- sql_mongo.py
+++ sql_mongo.py
@@ -38,6 +38,30 @@ elif(''.join((x for x in argv[1] if not x.isdigit())).lower().replace(" ", "") =
 
     else:
         print("given student_id does not exist")
+        
+# SELECT STUDENT_ID AS _id, COUNT(STUDENT_ID) AS Total Count FROM GRADE GROUP BY STUDENT_ID
+elif(argv[1].lower().replace(" ", "") == "select student_id as _id, count(student_id) as Total Count from grade group by student_id ".lower().replace(" ", "")):
+    #id = int(re.sub("\D", "", argv[1]))
+
+    agg_result= col.aggregate( 
+    [{ 
+    "$group" :  
+        {"_id" : "$student_id",  
+         "Total Count" : {"$sum" : 1},
+         }}
+    ])
+
+    s_id = []
+    total = []
+    for i in agg_result:
+        s_id.append(i['_id'])
+        total.append(i['Total Count'])
+    
+    df = pd.DataFrame({"student_id" : s_id, "Total Count" : total })
+    
+    df.to_csv('test.csv', index=False)
+    print("result is successfully saved on test.csv")
+    
 
 else:
     print("SQL query is not programmed!")
